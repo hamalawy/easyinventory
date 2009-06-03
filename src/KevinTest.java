b890f3570c495d624cff2b30b6427ebfd1928ad7
kruiz194 <kruiz194@61782fe2-4ed6-11de-aa6a-4703752c21e7>
2009-06-03 04:20:17 +0000

    Ok guys, as I'm sure you know, leaving comments is always a good idea.  This is my first commit to the repository, so disregard this as only a test.  Make sure to leave a comment every time you commit your code, so that way we are absolutely clear on what changes have been made.
    -Kev

---
+++ src/KevinTest.java
@@ -0,0 +1,13 @@
+import javax.swing.JOptionPane;
+
+
+public class KevinTest {
+
+	public static void main(String[] args) {
+		
+		System.out.println("This is a test! Hello World");
+		JOptionPane.showMessageDialog(null, "Play Him off, Keyboard Cat!");
+		JOptionPane.showMessageDialog(null, "I DEED IT!");
+		
+	}
+}
